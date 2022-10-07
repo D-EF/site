a77538671c4292f8dca480356cd4bcf404da809c
Darth_Eternalfaith <darth_ef@hotmail.com>
2022-10-07 17:49:45 +0800

    tempsave

--- js/index.js
+++ js/index.js
@@ -2,13 +2,17 @@
  * @Author: Darth_Eternalfaith darth_ef@hotmail.com
  * @Date: 2022-09-16 22:08:21
  * @LastEditors: Darth_Eternalfaith darth_ef@hotmail.com
- * @LastEditTime: 2022-09-16 22:42:51
+ * @LastEditTime: 2022-10-07 02:57:24
  * @FilePath: \site\js\index.js
  * @Description:  test
  * 
  * Copyright (c) 2022 by Darth_Eternalfaith darth_ef@hotmail.com, All Rights Reserved. 
  */
 
-import { test__matrix } from "./import/PrimitivesTGT/test/test_matrix";
+import { test__matrix } from "./import/PrimitivesTGT/test/test_matrix.js";
+
+console.time    ("test index.js");
 
 test__matrix();
+
+console.timeEnd ("test index.js");
