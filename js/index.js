ac03c271ea0a58537db17ad68c7929e87aa08c7c
Darth_Eternalfaith <darth_ef@hotmail.com>
2022-11-09 01:58:40 +0800

    temp save 2022-11-09

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
  * @Author: Darth_Eternalfaith darth_ef@hotmail.com
  * @Date: 2022-09-16 22:08:21
  * @LastEditors: Darth_Eternalfaith darth_ef@hotmail.com
- * @LastEditTime: 2022-10-07 02:57:24
+ * @LastEditTime: 2022-11-04 22:48:04
  * @FilePath: \site\js\index.js
  * @Description:  test
  * 
@@ -10,7 +10,9 @@
  */
 
 // import { test__matrix as test } from "./import/NML/test/test_matrix.js";
-import { test_rotate as test } from "./import/NML/test/test_rotate.js";
+// import { test_rotate as test } from "./import/NML/test/test_rotate.js";
+// import { test } from "./import/NML/test/test_vector.js";
+import { test } from "./import/NML/test/test_Transform_Matrix_Ctrl.js";
 
 console.time    ("test index.js");
 
