f281f02a8c9906caf99b904405a0dbcb1d691990
Darth_Eternalfaith <darth_ef@hotmail.com>
2022-12-18 23:36:58 +0800

    update submod

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
  * @Author: Darth_Eternalfaith darth_ef@hotmail.com
  * @Date: 2022-09-16 22:08:21
  * @LastEditors: Darth_Eternalfaith darth_ef@hotmail.com
- * @LastEditTime: 2022-11-04 22:48:04
+ * @LastEditTime: 2022-12-18 23:20:13
  * @FilePath: \site\js\index.js
  * @Description:  test
  * 
@@ -12,10 +12,11 @@
 // import { test__matrix as test } from "./import/NML/test/test_matrix.js";
 // import { test_rotate as test } from "./import/NML/test/test_rotate.js";
 // import { test } from "./import/NML/test/test_vector.js";
-import { test } from "./import/NML/test/test_Transform_Matrix_Ctrl.js";
+import { test as test_Vector } from "./import/NML/test/test_Vector.js";
+
 
 console.time    ("test index.js");
 
-test();
+test_Vector();
 
 console.timeEnd ("test index.js");
