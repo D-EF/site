b3d779696cc451def953b244df61fb38fe7943bb
Darth_Eternalfaith <darth_ef@hotmail.com>
2023-02-22 02:11:50 +0800

    update submod

--- js/index.js
+++ js/index.js
@@ -2,17 +2,16 @@
  * @Author: Darth_Eternalfaith darth_ef@hotmail.com
  * @Date: 2022-09-16 22:08:21
  * @LastEditors: Darth_Eternalfaith darth_ef@hotmail.com
- * @LastEditTime: 2023-02-15 11:55:32
+ * @LastEditTime: 2023-02-21 01:24:55
  * @FilePath: \site\js\index.js
  * @Description:  test
  * 
  * Copyright (c) 2022 by Darth_Eternalfaith darth_ef@hotmail.com, All Rights Reserved. 
  */
 
+// import { test as test_Vector } from "./import/NML/test/test__Vector.js";
 // import { test as test__Matrix } from "./import/NML/test/test__Matrix.js";
-// import { test_rotate as test } from "./import/NML/test/test_rotate.js";
-// import { test } from "./import/NML/test/test_vector.js";
-import { test as test_Vector } from "./import/NML/test/test__Vector.js";
+import { test as test_Basice } from "./import/Basics/test/test__basics.js";
 
 var {sqrt,sin,cos,tan,atan,atan2,asin,acos}=Math;
 var temp={sqrt,sin,cos,tan,atan,atan2,asin,acos};
@@ -21,7 +20,8 @@ Object.assign(window,temp);
 
 console.time("test index.js");
 
-test_Vector();
+// test_Vector();
 // test__Matrix();
+test_Basice();
 
 console.timeEnd ("test index.js");
